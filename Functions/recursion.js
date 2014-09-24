5c8f5d657b4dc4e51e4c187c10eb080ce9911e45
danielpetrarca <petrarca.11@osu.edu>
2014-09-23 23:16:54 -0500

    Edited Part 2 of the recursion problem

--- Functions/recursion.js
+++ Functions/recursion.js
@@ -35,15 +35,14 @@ console.log(isEven(75));
 //Fixing behavior on -1:
 
 function isEven (n) {
-	if (n<0) {
-		n = Math.abs(n);
-		return isEven(n);
-	}
 	if (n===0) {
 		return true;
 	}
 	else if (n===1) {
 		return false;
+	}
+	else if (n<0) {
+		return isEven(-n);
 	}
 	else {
 		return isEven(n-2);
